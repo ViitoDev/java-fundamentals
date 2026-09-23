6a0d43e35f9d0b0c61120d5e44a8edfa8651dc49
João Barros <joaovictorpiresdev@gmail.com>
2026-09-22 21:34:26 -0300

    chore: Add the synopsis

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-            System.out.printf("Welcome to ByteFlix");
+        System.out.printf("Welcome to ByteFlix\n");
 
-            String film = "Avengers: Endgame";
-            int releaseDate = 2019;
-            boolean planIncluded = true;
-            float filmRate = 8.4;
+        String film = "Avengers: Endgame";
+        int releaseDate = 2019;
+        boolean planIncluded = true;
+        double filmRate1 = 0;
+        double filmRate2 = 10;
+        double media = (filmRate1 + filmRate2) / 2;
+        String synopsis = """
+                          Synopsis : 
+                          After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must 
+                          reunite and assemble again to reinvigorate their trounced allies and restore balance.
+                          """;
+
+        System.out.println(synopsis);
+        System.out.println((int)media);
     }
 }
